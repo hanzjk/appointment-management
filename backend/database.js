b1cd6c1f730cc39d1367512bf93348f1eef39a70
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2025-10-03 15:30:10 +0530

    Switch database connection to CHOREO_XYZCON

--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.CHOREO_DBCON1_DATABASENAME,
-  process.env.CHOREO_DBCON1_USERNAME,
-  process.env.CHOREO_DBCON1_PASSWORD,
+  process.env.CHOREO_XYZCON_DATABASENAME,
+  process.env.CHOREO_XYZCON_USERNAME,
+  process.env.CHOREO_XYZCON_PASSWORD,
   {
-    host: process.env.CHOREO_DBCON1_HOSTNAME,
+    host: process.env.CHOREO_XYZCON_HOSTNAME,
     dialect: 'mysql',
-    port: process.env.CHOREO_DBCON1_PORT,
+    port: process.env.CHOREO_XYZCON_PORT,
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false // This ensures the connection uses SSL without verifying the server's certificate
